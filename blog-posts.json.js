0fc286d394fcb1d8407f4c1ab437113b8f1d19aa
daccio11 <139652889+daccio11@users.noreply.github.com>
2024-08-05 14:45:18 -0400

    Add files via upload

---
+++ blog-posts.json.js
@@ -0,0 +1,8 @@
+[
+    {
+        "title": "First Blog Post",
+        "date": "August 5, 2024",
+        "excerpt": "This is the content of my first blog post. Stay tuned for more updates!",
+        "url": "first-blog-post.html"
+    }
+]
